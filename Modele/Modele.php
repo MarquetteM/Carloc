2874d9fbe3f2aa309c6aaa81ed900fc35502378a
WadeL92 <127845156+WadeL92@users.noreply.github.com>
2023-04-25 10:27:11 +0200

    Add files via upload

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -1,2 +1,26 @@
-<?phpabstract class Modele {	private $bdd;	    protected function executerRequete($sql, $params = null) {		$getBdd = $this->getBdd();        if ($params == null){            $resultat = $getBdd->query($sql);        } else {            $resultat = $getBdd->prepare($sql);            $resultat->execute($params);        }        return $resultat;    }      private function getBdd() {		$dsn = "mysql:host=localhost;dbname=carloc";		$login = "root";		$mdp = "";        if ($this->bdd == null){            $this->bdd = new PDO($dsn,$login,$mdp,                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));        }        return $this->bdd;    }
+<?php
+abstract class Modele {
+	private $bdd;
+	
+    protected function executerRequete($sql, $params = null) {
+		$getBdd = $this->getBdd();
+        if ($params == null){
+            $resultat = $getBdd->query($sql);
+        } else {
+            $resultat = $getBdd->prepare($sql);
+            $resultat->execute($params);
+        }
+        return $resultat;
+    }
+  
+    private function getBdd() {
+		$dsn = "mysql:host=sioecorieqwade.mysql.db;dbname=sioecorieqwade;charset=utf8";
+		$login = "sioecorieqwade";
+		$mdp = "NXfeGwbIb9";
+        if ($this->bdd == null){
+            $this->bdd = new PDO($dsn,$login,$mdp,
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        return $this->bdd;
+    }
 }
